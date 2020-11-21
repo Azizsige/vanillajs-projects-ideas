4a2579412d4a5890d16690fbe1ad6f6f81c96c64
Azizsige <kurniawan00azizsige@gmail.com>
2020-11-21 10:46:06 +0700

    add next project

---
+++ project-2/app.js
@@ -0,0 +1,27 @@
+const wordsEl = document.getElementById('words');
+const lettersEl = document.getElementById('letters');
+
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+function getRemaining(){
+  const textValue = input.value;
+  const letters = textValue.length;
+
+  lettersEl.textContent = `${letters}`;
+
+  const words = textValue.split(" ");
+
+  const wordsCount = words[words.length - 1] === "" ? words.length - 1 : words.length;
+
+  wordsEl.textContent = `${wordsCount}`
+}
+
+input.addEventListener('input', getRemaining);
+
+button.addEventListener('click', () => {
+  wordsEl.textContent = '0'
+  lettersEl.textContent = '0';
+  input.value = ''
+
+})
