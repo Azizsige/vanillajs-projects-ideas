90a4f8d984fe25c9f04c3f7d47541951a8e7b87e
Azizsige <kurniawan00azizsige@gmail.com>
2020-12-09 13:52:10 +0700

    change animation in project image slider

--- project-4/app.js
+++ project-4/app.js
@@ -2,37 +2,44 @@ index = 1;
 
 showSlide(index);
 
-function plusSlide(n){
+function plusSlide(n) {
   showSlide(index = index + n);
+  const img = document.getElementsByClassName('slide');
+
+
+  for (let i = 0; i < img.length; i++) {
+    img[i].classList.add('right');
+  }
 }
 
-function currentSlide(n){
+function currentSlide(n) {
   showSlide(index = n);
 }
 
-function showSlide(n){
+function showSlide(n) {
   let slides = document.getElementsByClassName('slide');
   let dots = document.getElementsByClassName('dot');
 
   let i;
 
-  if(n < 1){
+  if (n < 1) {
     index = slides.length;
   }
 
-  if(n > slides.length){
+  if (n > slides.length) {
     index = 1;
   }
 
-  for(i = 0; i<slides.length; i++){
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"
   }
 
-  for(i = 0; i<dots.length; i++){
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
   }
 
-  slides[index-1].style.display = "block";
-  dots[index-1].className += " active";
+  slides[index - 1].style.display = "block";
+  dots[index - 1].className += " active"
 
 }
+
